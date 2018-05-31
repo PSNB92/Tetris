8d11303646387e612935f9468cf3ccd6d4d4cd3e
cdruk <cdrukier@student.touro.edu>
2018-05-31 15:46:10 -0400

    button press failing

--- test/org/psnbtech/TetrisKeyAdapterTest.java
+++ test/org/psnbtech/TetrisKeyAdapterTest.java
@@ -3,21 +3,26 @@ package org.psnbtech;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.event.KeyEvent;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class TetrisKeyAdapterTest {
 
 	@Test
-	public void testKeyPressed() {
-		//given mock 
+	public void testDownKeyPressed() {
+		//given 
 		Tetris tetris = Mockito.mock(Tetris.class);
 		TetrisKeyAdapter tetrisKeyAdapter = new TetrisKeyAdapter(tetris);
 		
-		
 		//when button pressed
-		
+		@SuppressWarnings("deprecation")
+		KeyEvent e = new KeyEvent(tetris, 0, 0, KeyEvent.VK_DOWN, 0);
+		tetrisKeyAdapter.keyPressed(e);
+
 		//then method tetris called
+		Mockito.	verify(tetris.getLogicTimer());
 	}
 
 }
