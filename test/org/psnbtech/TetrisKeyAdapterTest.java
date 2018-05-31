1df734a14fa7197671675f59e41f4b6efa6e5419
cdruk <cdrukier@student.touro.edu>
2018-05-31 18:09:43 -0400

    two working tests

--- test/org/psnbtech/TetrisKeyAdapterTest.java
+++ test/org/psnbtech/TetrisKeyAdapterTest.java
@@ -1,26 +1,74 @@
 package org.psnbtech;
 
 
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 import java.awt.event.KeyEvent;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-class TetrisKeyAdapterTest {
+public class TetrisKeyAdapterTest {
+	
+	@Mock
+	Tetris tetris;
+	@Mock
+	Clock clock;
+	@Mock
+	BoardPanel boardPanel;
+	@Mock
+	KeyEvent keyEvent;
+	
+	TetrisKeyAdapter tetrisKeyAdapter;
+
+	@BeforeEach
+	public void before() {
+		initMocks(this);
+		tetrisKeyAdapter = new TetrisKeyAdapter(tetris);
+		when(tetris.getLogicTimer()).thenReturn(clock);
+		doReturn(boardPanel).when(tetris).getBoard();
+	}
 
 	@Test
 	public void testDownKeyPressed() {
 		//given 
-		Tetris tetris = Mockito.mock(Tetris.class);
-		TetrisKeyAdapter tetrisKeyAdapter = new TetrisKeyAdapter(tetris);
-		KeyEvent e = Mockito.mock(KeyEvent.class);
-		Mockito.when(e.getKeyCode()).thenReturn(KeyEvent.VK_DOWN);
+		doReturn(false).when(tetris).isPaused();
+		doReturn(0).when(tetris).getDropCooldown();
+		when(keyEvent.getKeyCode()).thenReturn(KeyEvent.VK_DOWN);
 		
-		//when button pressed
-		tetrisKeyAdapter.keyPressed(e);
+		//when 
+		tetrisKeyAdapter.keyPressed(keyEvent);
 
-		//then method tetris called
-		Mockito.	verify(tetris.getLogicTimer());
+		//then
+		verify(tetris).isPaused();
+		verify(tetris).getDropCooldown();
+		verify(clock).setCyclesPerSecond(25.0f);
+	}
+
+	@Test
+	public void testLeftKeyPressed() {
+		//given 
+		TetrisKeyAdapter tetrisKeyAdapter = new TetrisKeyAdapter(tetris);
+		doReturn(false).when(tetris).isPaused();
+		Mockito.when(boardPanel.isValidAndEmpty(Mockito.any(), Mockito.anyInt(), Mockito.anyInt(), Mockito.anyInt())).thenReturn(true);
+		Mockito.when(keyEvent.getKeyCode()).thenReturn(KeyEvent.VK_LEFT);
+		
+		//when 
+		tetrisKeyAdapter.keyPressed(keyEvent);
+
+		//then 
+		Mockito.verify(tetris).isPaused();
+		Mockito.verify(tetris).getBoard();
+		Mockito.verify(tetris).decrementCurrentCol();
 	}
 
 }
+
